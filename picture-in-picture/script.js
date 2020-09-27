0d874eb1aa730c07d44116e05ccebe2cc2d97255
Salman Said Ramadhan <salman.1202@gmail.com>
2020-09-27 14:07:03 +0700

    finishing picture-in-picture

---
+++ picture-in-picture/script.js
@@ -0,0 +1,26 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play 
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        console.log('whooops, error here: ', error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+selectMediaStream()
