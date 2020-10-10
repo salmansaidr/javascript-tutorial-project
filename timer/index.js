8294ca1b5c573862a5cc80d2530ed54671064cec
Salman <salman.1202@gmail.com>
2020-10-10 07:11:46 +0700

    timer project svg

--- timer/index.js
+++ timer/index.js
@@ -1,19 +1,15 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click', this.start);
-    }
-
-    start() {
-        console.log('Time to start the timer!');
-    }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log('Timer Started!');
+    },
+    onTick() {
+        console.log('Timer just ticked down');
+    },
+    onComplete() {
+        console.log('Timer is completed');
+    }
+});
